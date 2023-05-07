fa89839f186c080043494a3c6850eadf66db21a2
Polya <poina.ustyugova.04@mail.ru>
2023-05-07 19:02:08 +0000

    Class-10

---
+++ Классы 10/Pair.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std; 
+class Pair
+{
+	int first; 
+	double second;
+public:
+	Pair() 
+	{
+		first = 0; second = 0;
+	}
+	Pair(int f, double s) { first = f; second = s; }
+	Pair(const Pair& p) { first = p.first; second = p.second; }
+	~Pair() {};
+	Pair operator =(const Pair&);
+	Pair operator -(const Pair& p);
+	Pair& operator +(const int x);
+	Pair& operator +(const double x);
+	bool operator <(const Pair&); // для сравнения чисел и выполнения условия
+	friend istream& operator>>(istream& in, Pair& p);
+	friend ostream& operator<<(ostream& out, const Pair& p);
+	// для работы с файлом
+	friend fstream& operator>>(fstream& fin, Pair& p);
+	friend fstream& operator<<(fstream& fout, const Pair& p);
+};
